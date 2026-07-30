ec99d71bc10cf95c3f9d44275e3d1e5f1d0b6162
HenriMIgueldaSilva <silvahenrimiguel@gmail.com>
2026-07-30 15:46:56 -0300

    localização 07/30

--- static/js/localizacao.js
+++ static/js/localizacao.js
@@ -1,28 +1,54 @@
-const modal = document.getElementById("modal-localizacao");
+navigator.geolocation.getCurrentPosition(
+    sucesso,
+    erro,
+    {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0
+    }
+);
 
-document.getElementById("permitir").onclick = () => {
+function sucesso(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
 
-    navigator.geolocation.getCurrentPosition(
-        (position)=>{
-
-            console.log(position.coords.latitude);
-            console.log(position.coords.longitude);
-
-            modal.style.display = "none";
-
-        },
-
-        (erro)=>{
-
-            alert("Não foi possível acessar sua localização.");
-
-        }
-    );
-
+    console.log(latitude, longitude);
 }
 
-document.getElementById("negar").onclick = ()=>{
+function erro(error){
+    console.log(error.message);
+}
 
-    modal.style.display = "none";
+const map = L.map('map').setView([-21.6033, -48.3658], 15);
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+
+if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        map.setView([latitude, longitude], 16);
+
+        L.marker([latitude, longitude])
+            .addTo(map)
+            .bindPopup("Você está aqui.")
+            .openPopup();
+
+    }, function () {
+
+        alert("Não foi possível obter sua localização.");
+
+    });
+
+} else {
+
+    alert("Seu navegador não suporta Geolocalização.");
 
 }
