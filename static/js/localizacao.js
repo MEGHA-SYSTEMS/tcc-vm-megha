c40ebea930f566e1819bd4a7838077ae70f4da08
HenriMIgueldaSilva <silvahenrimiguel@gmail.com>
2026-07-30 17:53:05 -0300

    localização formatação 07/30

--- static/js/localizacao.js
+++ static/js/localizacao.js
@@ -8,18 +8,21 @@ navigator.geolocation.getCurrentPosition(
     }
 );
 
-function sucesso(position){
+function sucesso(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
     console.log(latitude, longitude);
 }
 
-function erro(error){
+function erro(error) {
     console.log(error.message);
 }
 
-const map = L.map('map').setView([-21.6033, -48.3658], 15);
+const map = L.map('map', {
+    zoomControl: false,
+    attributionControl: false
+}).setView([-21.6033, -48.3658], 15);
 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -50,5 +53,42 @@ if (navigator.geolocation) {
 } else {
 
     alert("Seu navegador não suporta Geolocalização.");
+
+}
+
+document.getElementById("buscar").addEventListener("click", buscarEndereco);
+
+async function buscarEndereco(){
+
+    const bairro = document.getElementById("bairro").value;
+    const rua = document.getElementById("rua").value;
+    const numero = document.getElementById("numero").value;
+
+    const endereco =
+        `${rua} ${numero}, ${bairro}, Matão, SP`;
+
+    const resposta = await fetch(
+        `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(endereco)}`
+    );
+
+    const dados = await resposta.json();
+
+    if(dados.length == 0){
+
+        alert("Endereço não encontrado.");
+
+        return;
+
+    }
+
+    const lat = dados[0].lat;
+    const lon = dados[0].lon;
+
+    map.setView([lat, lon],17);
+
+    L.marker([lat,lon])
+        .addTo(map)
+        .bindPopup(endereco)
+        .openPopup();
 
 }
