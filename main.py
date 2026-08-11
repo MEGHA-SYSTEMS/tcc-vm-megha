e8d3c608f81153e01113c71646dc635446dacfbc
Luis <luis.santos585@etec.sp.gov.br>
2026-08-11 17:09:02 -0300

    correcao

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def localizacao():
     return render_template('pages/localizacao.html')
 
 def main():
-    app.run(port=int(os.environ.get("PORT", 80)))
+    app.run(host="0.0.0.0", port = int(os.environ.get("PORT", 10000)))
 
 if __name__ == "__main__":
     main()
