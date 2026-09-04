8266207212e130edec569550faba1a3746aa67da
alanvictorb <alanvictorbatista729@gmail.com>
2026-09-04 17:34:35 -0300

    Finalização dos conectores de dados

--- app/models/farmacia.py
+++ app/models/farmacia.py
@@ -1,9 +1,10 @@
 from dataclass import dataclass
+from datatime import datetime
 
 @dataclass
 class user:
     uid:str
     nome:str
     email:str
-    created_at: str = None
+    created_at: str = datatime = field(default_factory=datatime.utcnow)
     updated_at: str = None
